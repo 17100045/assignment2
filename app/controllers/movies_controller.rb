88313dcba708b0556634740c5da06364ee3449eb
a17100045 <17100045@lums.edu.pk>
2016-02-27 20:26:28 +0000

    commit something

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,9 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+  def movie_params2
+    params.require(:movie).permit(:oldtitle, :title, :rating, :description, :release_date)
+  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -37,12 +40,12 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find params[:id]
-    if((movie_params[:title]).blank? || movie_params[:release_date].blank? || movie_params[:rating].blank?)
-      flash[:notice] = "error fields empty"
-      redirect_to movies_path
-      return
-    end
+    #@movie = Movie.find params[:id]
+    #if((movie_params[:title]).blank? || movie_params[:release_date].blank? || movie_params[:rating].blank?)
+    #  flash[:notice] = "error fields empty"
+    #  redirect_to movies_path
+    #  return
+    #end
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
@@ -52,6 +55,63 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  def deletemovie
+    #if !(params[:title].nil?)
+    #  @movie = Movie.find(params[:title])
+    #  flash[:notice] = "Movie '#{@movie.title}' deleted."
+    #  redirect_to movies_path
+    #  @movie.destroy
+    #  return
+    #end
+  end
+  def updatemov
+    #@movie = Movie.find_by movie_params2[:tmp]
+    
+  #  if((movie_params[:title]).blank? || movie_params[:release_date].blank? || movie_params[:rating].blank?)
+  #    flash[:notice] = "error fields empty"
+   #   redirect_to movies_path
+  #    return
+  #  end
+  #  @movie.update_attributes!(movie_params)
+  #  flash[:notice] = "#{@movie.title} was successfully updated."
+  #  redirect_to movie_path(@movie)
+  end
+  def update_mov
+    @movie = Movie.find_by movie_params2[:tmp]
+    
+    if((movie_params2[:title]).blank? || movie_params2[:release_date].blank? || movie_params2[:rating].blank?)
+      flash[:notice] = "error fields empty"
+      redirect_to movies_path
+      return
+    end
+    @movie.update_attributes!(movie_params2)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  def delete_mov
+
+    @movies = Movie. all
+    args = params.require(:movie).permit(:title, :rating)
+    if !((args[:rating]).blank?)
+      @movies = @movies.where(:rating => args[:rating])
+      @movies.all.each do |i|
+        i.destroy
+      redirect_to movies_path
+    end
+    return
+    elsif !((args[:title]).blank?)
+      @movie = Movie.find_by_title(args[:title])
+      flash[:notice] = "Movie '#{@movie.title}' deleted."
+      @movie.destroy
+      redirect_to movies_path
+      return
+    
+    end
+    flash[:notice] = "both left blank"
+    redirect_to movies_path
+
+      
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
+  get 'movies/deletemovie' => 'movies#deletemovie'
+  put '/movies/deletemovie(.:format)' => 'movies#delete_mov'
+  
+  get 'movies/updatemov' => 'movies#updatemov'
 
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
