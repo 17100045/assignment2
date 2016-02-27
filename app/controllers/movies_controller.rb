30efc04af67eaa6e8da7f032fe7584a52fe47faa
a17100045 <17100045@lums.edu.pk>
2016-02-27 20:26:08 +0000

    commit 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,9 +9,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
-  def index
+  def index 
     @movies = Movie.all
+    @all_ratings = Movie.get_possible_ratings
+    if params[:sort_by] == "title"
+      @movies = @movies.sort_by &:title
+    elsif params[:sort_by] == "release_date"
+      @movies = @movies.sort_by &:release_date
+    end
+    if !(params[:ratings].nil?)
+      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    end
   end
 
   def new
@@ -30,11 +38,15 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
+    if((movie_params[:title]).blank? || movie_params[:release_date].blank? || movie_params[:rating].blank?)
+      flash[:notice] = "error fields empty"
+      redirect_to movies_path
+      return
+    end
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
-
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
